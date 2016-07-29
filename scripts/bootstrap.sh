a81bd7785a9dd743b619728a9076cab1518bec6b
Zachary Rump <nihilanth41@gmail.com>
2016-07-29 12:30:31 -0500

    Build git from source

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -10,16 +10,14 @@ if [ ! -d "$DOWNLOAD_DIR" ]; then
   mkdir -p "$DOWNLOAD_DIR"
 fi
 
-yum -y install git vim-enhanced wget gcc kernel-devel make ncurses-devel man man-pages
+yum -y install vim-enhanced wget gcc kernel-devel make ncurses-devel man man-pages epel-release yum-utils
+yum -y install libcurl-devel perl-ExtUtils-MakeMaker.x86_64 gettext
 
-cd /etc/yum.repos.d/
-wget http://download.opensuse.org/repositories/utilities/CentOS_6/utilities.repo
+curl http://download.opensuse.org/repositories/utilities/CentOS_6/utilities.repo -O /etc/yum.repos.d/utilities.repo
 yum -y install rcm
 
-# Tmux 1.8
 
 # Libevent2
-
 if [ ! -f "$DOWNLOAD_DIR/libevent-2.0.22-stable.tar.gz" ]; then
 	wget -q -O "/tmp/libevent-2.0.22-stable.tar.gz" "https://github.com/libevent/libevent/releases/download/release-2.0.22-stable/libevent-2.0.22-stable.tar.gz"
 else
@@ -32,6 +30,7 @@ cd "libevent-2.0.22-stable"
 ./configure --prefix=/usr/local 
 make && make install
 
+# Tmux 
 if [ ! -f "$DOWNLOAD_DIR/tmux-2.2.tar.gz" ]; then
 	wget -q -O "/tmp/tmux-2.2.tar.gz" "https://github.com/tmux/tmux/releases/download/2.2/tmux-2.2.tar.gz"
 else
@@ -43,6 +42,22 @@ tar -xvzf "tmux-2.2.tar.gz"
 cd "tmux-2.2"
 LDFLAGS="-L/usr/local/lib -Wl,-rpath=/usr/local/lib" ./configure --prefix=/usr/local
 make && make install
+
+# Git
+GIT_VERSION="2.9.2"
+if [ ! -f "$DOWNLOAD_DIR/git-$GIT_VERSION.tar.gz" ]; then
+	wget -q -O "/tmp/git-$GIT_VERSION.tar.gz" "https://github.com/git/git/archive/v$GIT_VERSION.tar.gz"
+else
+	cp "$DOWNLOAD_DIR/git-$GIT_VERSION.tar.gz" "/tmp/git-$GIT_VERSION.tar.gz"
+fi
+
+cd /tmp
+tar -xvzf "git-$GIT_VERSION.tar.gz"
+cd "git-$GIT_VERSION"
+./configure 
+make prefix=/usr/local all NO_TCLTK=YesPlease
+make prefix=/usr/local install 
+
 
 
 
